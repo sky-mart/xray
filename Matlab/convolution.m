88f11b4af6a6eeec105c674978644cdbbb210689
sky-mart <sky-mart@hotmail.com>
2016-10-28 18:06:49 +0400

    Method to avoid zero elements in DFT

--- Matlab/convolution.m
+++ Matlab/convolution.m
@@ -6,11 +6,15 @@ psf = ones(k,k) / k^2;
 
 conv_sz = size(src) + k - 1;
 
-if mod(conv_sz(1),k) == 0
-    src = padarray(src, [1, 0], 0, 'post');
-end
-if mod(conv_sz(2),k) == 0
-    src = padarray(src, [0, 1], 0, 'post');
+% for every n: GCD(N,K) * GCD(N,n) != N
+% consequently, GCD(N,K) == 1
+N = conv_sz(1);
+while 1
+    if (gcd(N, k) == 1)
+        src = padarray(src, N - conv_sz, 0, 'post');    
+        break;
+    end
+    N = N + 1;
 end
 
 c = conv2(src, psf);
--- Matlab/zero_dft.m
+++ Matlab/zero_dft.m
@@ -1,9 +1,27 @@
 
 
-for M = 2:100
-    s = 0;
-    for m = 0:M-1
-        s = s + exp(2*pi*m*1i/M);
+% for M = 2:100
+%     s = 0;
+%     for m = 0:M-1
+%         s = s + exp(2*pi*m*1i/M);
+%     end
+%     disp(s);
+% end
+
+N = 15;
+K = 6;
+n = 5;
+
+for m = 0:N-1
+    s1 = 0;
+    for k = 0:K-1
+        s1 = s1 + exp(1i*2*pi*n*k/N);
     end
-    disp(s);
+    s2 = 0;
+    for l = 0:K-1
+        s2 = s2 + exp(1i*2*pi*l*m/N);
+    end
+    s = s1*s2;
+    disp([m, s1, s2, s])
 end
+
